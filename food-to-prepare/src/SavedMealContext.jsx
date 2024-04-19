0743aaf6fef226ab832315eed57787d065681811
aaade_pm <olatunjiademolaimmanuel@gmail.com>
2024-04-19 23:15:18 +0100

    Remove meals from saved meals list

--- food-to-prepare/src/SavedMealContext.jsx
+++ food-to-prepare/src/SavedMealContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const SavedMealContext = createContext();
 
 const SavedMealProvider = ({ children }) => {
   const [savedMeals, setSavedMeals] = useState([]);
+
+  useEffect(() => {
+    const savedMealsData = localStorage.getItem("savedMeals");
+    if (savedMealsData) {
+      setSavedMeals(JSON.parse(savedMealsData));
+    }
+  }, []);
 
   const saveMeal = (mealToAdd) => {
     if (savedMeals.some((meal) => meal.name === mealToAdd.name)) {
@@ -13,9 +20,24 @@ const SavedMealProvider = ({ children }) => {
     }
 
     setSavedMeals([...savedMeals, mealToAdd]);
+    localStorage.setItem(
+      "savedMeals",
+      JSON.stringify([...savedMeals, mealToAdd])
+    );
+  };
+
+  const removeMeal = (mealToRemove) => {
+    const updatedMeals = savedMeals.filter(
+      (meal) => meal.name !== mealToRemove.name
+    );
+    setSavedMeals(updatedMeals);
+    localStorage.setItem("savedMeals", JSON.stringify(updatedMeals));
+    console.log("remove meal");
   };
   return (
-    <SavedMealContext.Provider value={{ saveMeal, savedMeals, setSavedMeals }}>
+    <SavedMealContext.Provider
+      value={{ saveMeal, savedMeals, setSavedMeals, removeMeal }}
+    >
       {children}
     </SavedMealContext.Provider>
   );
--- food-to-prepare/src/components/FavoriteMeals.jsx
+++ food-to-prepare/src/components/FavoriteMeals.jsx
@@ -1,23 +1,39 @@
 import { SavedMealContext } from "../SavedMealContext";
 import { useContext } from "react";
+import { ImBin } from "react-icons/im";
 
 const FavoriteMeals = () => {
-  const { savedMeals } = useContext(SavedMealContext);
+  const { savedMeals, removeMeal } = useContext(SavedMealContext);
+  const handleRemoveMeal = (meal) => {
+    const mealToRemove = meal;
+    removeMeal(mealToRemove);
+  };
+
   return (
     <div>
-      {savedMeals.length > 0 &&
+      {savedMeals.length > 0 ? (
         savedMeals.map((meal, index) => (
           <div key={index} className="favorite-card">
             <div className="favorite-image">
               <img src={meal.image} alt="" />
             </div>
             <div className="favorite-content">
-              <p>{meal.name}</p>
-              <p>{meal.category}</p>
-              <p>{meal.area}</p>
+              <div className="favorite-text">
+                <p>{meal.name}</p>
+                <p>{meal.category}</p>
+                <p>{meal.area}</p>
+              </div>
+              <div className="favorite-icon">
+                <ImBin onClick={() => handleRemoveMeal(meal)} />
+              </div>
             </div>
           </div>
-        ))}
+        ))
+      ) : (
+        <div className="no-favorite">
+          <h2>No saved meals</h2>
+        </div>
+      )}
     </div>
   );
 };
